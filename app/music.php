ceb8513f56da0b83d7a56b95aedc6e84070b348a
mybabyisapassword <129019478+mybabyisapassword@users.noreply.github.com>
2023-06-04 23:40:52 +0800

    Add files via upload

---
+++ app/music.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>music</title>
+</head>
+<body>
+    <iframe width="0" height="0" src="https://www.youtube.com/embed/QMokMQ8Bu7Y?rel=0&autoplay=1" frameborder="0" allow="autoplay"></iframe>
+</body>
+</html>
